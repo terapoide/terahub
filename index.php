b77a91699e249ce8531a914d9a65dc6e4b47efe0
terapoide <93676840+terapoide@users.noreply.github.com>
2023-05-24 17:07:02 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>Hub</title>
+</head>
+<body>
+    <fieldset class="fieldprincipal">  
+        <h2>TERAHUB <br> </h2>
+        <menu>
+        <nav id="menu-h">
+            <ul>
+                <li><a href="deepwoken/copybut.php" class="link">Charisma Deepwoken</a></li> 
+            <ul>
+        </nav>  
+</menu>
+    </fieldset>
+</body>
+</html>
